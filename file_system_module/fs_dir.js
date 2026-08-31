79135c2540100399f254e7b6a4f37c229e2f239b
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2026-08-31 10:26:15 +0530

    mkdir files

--- file_system_module/fs_dir.js
+++ file_system_module/fs_dir.js
@@ -1,7 +1,7 @@
 const fs = require("fs")                               // callback based method
 
-fs.mkdir("./myFolder", (err) => {          // create a folder method 1
-// fs.mkdir("./myFolder1/myFolder2/myFolder3", { recursive: true }, (err) => {          // create a folder
+// fs.mkdir("./myFolder", (err) => {          // create a folder method 1
+fs.mkdir("./myFolder1/myFolder2/myFolder3", { recursive: true }, (err) => {          // create a folder
     if (err) {
         console.log(err);
         return;

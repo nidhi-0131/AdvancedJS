a322328a7a74193729ad30f739f4c7843bf1d016
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2026-08-31 09:38:47 +0530

    crud sync async

--- file_system_module/crud_async.js
+++ file_system_module/crud_async.js
@@ -13,10 +13,14 @@ fs.readFile("notes.txt", (err, data) => {             // read the data from the 
         console.error(err);
         return;
     }
-    console.log(`read data: ${data}`);
+    console.log(`read data: ${data}`);    // not using data instead of `read data: ${data}` because data is a buffer and will be converted to string automatically
 });
 
 fs.appendFile("notes.txt", " Hello Node.js", (err) => {    // append data to the file
+    if (err) {
+        console.error(err);
+        return;
+    }
     console.log("Data successfully appended to the file notes.txt");
 })
 

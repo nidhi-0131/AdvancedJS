ebee4daa0cc2db4932b59022f3112c402129ff0c
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2026-09-15 13:02:10 +0530

    implement routing

---
+++ http_module/routing.js
@@ -0,0 +1,20 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    // console.log('Hello World!');
+    // console.log(req.url);
+    // res.end("Hello from Server")
+    if(req.url === "/") {
+    res.end("Hello from Home Page")
+    } else if (req.url === "/about") {
+        res.end("Hello from About Page")
+    } else if (req.url === "/contact") {
+        res.end("Hello from Contact Page")
+    } else {
+        res.end("404 Page Not Found")
+    }
+})
+
+server.listen(3001, "127.0.0.1", () => {
+    console.log("Server is running on http://127.0.0.1/3001/");
+})

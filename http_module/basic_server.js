ee29c2cf93b0f498935a9927c832fd9110e3b116
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2026-09-15 12:09:53 +0530

    read data from index.html in the server

--- http_module/basic_server.js
+++ http_module/basic_server.js
@@ -1,8 +1,9 @@
 import http from 'http';
-import { readFile } from 'node:fs';
+import { readFileSync } from 'node:fs';
 
 // creating a basic server using http module
 
+const data = readFileSync("index.html", "utf-8");
 const server = http.createServer((req, res) => {
     console.log("Hello World!");
     const order = {
@@ -13,20 +14,21 @@ const server = http.createServer((req, res) => {
     }
    // res.statusCode = 200;
    // res.setHeader('Content-Type', 'application/json');
+    console.log(`${data}`);
     res.writeHead(200, {
-        "Content-Type": "application/json", "custom-header": "Hello ECE"
+        "Content-Type": "text/html", "custom-header": "Hello ECE"
     }); // only in single thing instead of above two separately
-    res.end(JSON.stringify(order));
+    res.end(data);
 })
 
 server.listen(3000, "127.0.0.1", () => {
     console.log("Server is running ...");
 })
 
-readFile("index.html", (err, data) => {
-    if (err) {
-        console.log(err.message)
-        return;
-    }
-    console.log('read data: ', data.toString());
-})
+// readFile("index.html", "utf-8", (err, data) => {
+//     if (err) {
+//         console.log(err.message)
+//     } else {
+//         console.log(data);
+//     }
+// })

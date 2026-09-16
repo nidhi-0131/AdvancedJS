50576bb29fe79f304035d2771ad5febd6425b0f3
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2026-09-16 17:06:01 +0530

    implemented routing

--- http_module/routing.js
+++ http_module/routing.js
@@ -1,11 +1,13 @@
 import http from 'http';
+import fs from "fs";
 
+const homePage = fs.readFileSync("home.html", "utf-8");
 const server = http.createServer((req, res) => {
     // console.log('Hello World!');
     // console.log(req.url);
     // res.end("Hello from Server")
     if(req.url === "/") {
-    res.end("Hello from Home Page")
+        res.end(homePage.replace("{{%CONTENT%}}", "Hello from Home Page"))
     } else if (req.url === "/about") {
         res.end("Hello from About Page")
     } else if (req.url === "/contact") {
@@ -16,5 +18,5 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(3001, "127.0.0.1", () => {
-    console.log("Server is running on http://127.0.0.1/3001/");
+    console.log("Server is running on http://127.0.0.1:3001/");
 })
